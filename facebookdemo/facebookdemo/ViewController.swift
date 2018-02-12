cbdea205092822b8f5a11a0f97a28cf6ea6a286c
lyj <lyj@lyjs-Mac.local>
2018-02-12 00:19:50 -0800

    lyj

---
+++ facebookdemo/facebookdemo/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  facebookdemo
+//
+//  Created by lyj on 2/12/18.
+//  Copyright © 2018 lyj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func click(_ sender: Any) {
+        self.label.text="hello";
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
